5d0c8cd176932da07d6192420af98f27fc0840e9
Noah- Yannick Schmid <schmid.noah@outlook.com>
2023-05-10 21:45:14 +0200

    Initial commit

---
+++ Debug/empty.d
@@ -0,0 +1,117 @@
+# FIXED
+
+empty.obj: ../empty.c
+empty.obj: C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/posix/ccs/unistd.h
+empty.obj: C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/posix/ccs/sys/types.h
+empty.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/stddef.h
+empty.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_ti_config.h
+empty.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/linkage.h
+empty.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/stdint.h
+empty.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_stdint40.h
+empty.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/stdint.h
+empty.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/cdefs.h
+empty.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_types.h
+empty.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_types.h
+empty.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_stdint.h
+empty.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_stdint.h
+empty.obj: C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/posix/ccs/sys/_internal.h
+empty.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/time.h
+empty.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/abi_prefix.h
+empty.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/xlocale/_time.h
+empty.obj: C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/drivers/GPIO.h
+empty.obj: ../Board.h
+empty.obj: C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/drivers/Board.h
+empty.obj: ../CC1350_LAUNCHXL.h
+empty.obj: C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/drivers/PIN.h
+empty.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/stdbool.h
+empty.obj: C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/devices/cc13x0/driverlib/ioc.h
+empty.obj: C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/devices/cc13x0/inc/hw_types.h
+empty.obj: C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/devices/cc13x0/inc/hw_chip_def.h
+empty.obj: C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/devices/cc13x0/inc/hw_memmap.h
+empty.obj: C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/devices/cc13x0/inc/hw_ioc.h
+empty.obj: C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/devices/cc13x0/inc/hw_ints.h
+empty.obj: C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/devices/cc13x0/driverlib/interrupt.h
+empty.obj: C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/devices/cc13x0/inc/hw_nvic.h
+empty.obj: C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/devices/cc13x0/driverlib/debug.h
+empty.obj: C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/devices/cc13x0/driverlib/cpu.h
+empty.obj: C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/devices/cc13x0/inc/hw_cpu_scs.h
+empty.obj: C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/devices/cc13x0/driverlib/rom.h
+empty.obj: C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/devices/cc13x0/driverlib/gpio.h
+empty.obj: C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/devices/cc13x0/inc/hw_gpio.h
+
+../empty.c:
+
+C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/posix/ccs/unistd.h:
+
+C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/posix/ccs/sys/types.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/stddef.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_ti_config.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/linkage.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/stdint.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_stdint40.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_types.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_types.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_stdint.h:
+
+C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/posix/ccs/sys/_internal.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/time.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/abi_prefix.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/xlocale/_time.h:
+
+C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/drivers/GPIO.h:
+
+../Board.h:
+
+C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/drivers/Board.h:
+
+../CC1350_LAUNCHXL.h:
+
+C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/drivers/PIN.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/stdbool.h:
+
+C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/devices/cc13x0/driverlib/ioc.h:
+
+C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/devices/cc13x0/inc/hw_types.h:
+
+C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/devices/cc13x0/inc/hw_chip_def.h:
+
+C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/devices/cc13x0/inc/hw_memmap.h:
+
+C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/devices/cc13x0/inc/hw_ioc.h:
+
+C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/devices/cc13x0/inc/hw_ints.h:
+
+C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/devices/cc13x0/driverlib/interrupt.h:
+
+C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/devices/cc13x0/inc/hw_nvic.h:
+
+C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/devices/cc13x0/driverlib/debug.h:
+
+C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/devices/cc13x0/driverlib/cpu.h:
+
+C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/devices/cc13x0/inc/hw_cpu_scs.h:
+
+C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/devices/cc13x0/driverlib/rom.h:
+
+C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/devices/cc13x0/driverlib/gpio.h:
+
+C:/ti/simplelink_cc13x0_sdk_4_20_02_07/source/ti/devices/cc13x0/inc/hw_gpio.h:
+
